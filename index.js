054c3a02f7a47418fc3c47f8b9ade4aa87cea3fb
mattfaircloth <mattcfaircloth@gmail.com>
2018-01-13 08:51:26 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 const app = "I don't do much.";
+
+fetch('https://api.github.com/repos/jquery/jquery/commits')
+  .then(res => res.json())
+  .then(json => console.log(json));
+
+  fetch('https://api.github.com/user/repos', {
+    headers: {
+      Authorization: `token ${token}`
+    }
+  }).then(res => res.json()).then(json => console.log(json));
